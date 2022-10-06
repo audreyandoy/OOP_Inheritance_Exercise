6210c4ca2e9c0ed243dcda67f82ea4c5bd8e62ea
Audrey <audreylandoy@gmail.com>
2022-10-06 07:44:12 -0700

    deleted venv

--- main.py
+++ main.py
@@ -2,10 +2,3 @@ from classes.vehicle import Vehicle
 from classes.car import Car
 from classes.airplane import Airplane
 
-car = Car("toyota", "camry", 2020, 200)
-car.display_speed()
-
-commercial_airplane = Airplane("Boeing", "747", 975000, 416)
-commercial_airplane.take_off()
-
-# max_capacity, wheels, name, manufacturer):
